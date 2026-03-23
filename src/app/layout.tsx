d62060b511b1e761f50c5fba5ae963b06bbb2f8e
Ana Luiza <anatibiricapaixao@outlook.com>
2026-03-23 00:42:27 -0300

    11. Adicionando Footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,43 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="pt-BR" className={inter.variable}>
-      <body>{children}</body>
+      <body>
+
+        {children}
+
+        <footer className="footer">
+          <span className="footerSignature">Ana Luiza Tibiriçá da Paixão</span>
+
+          <div className="footerLinks">
+            <a
+              href="https://www.linkedin.com/in/ana-luiza-tibirica-da-paixao/"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="footerImageLink"
+            >
+              <img
+                src="/assets/linkedin.png"
+                alt="LinkedIn"
+                className="footerImage"
+              />
+            </a>
+
+            <a
+              href="https://github.com/Naluaqui"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="footerImageLink"
+            >
+              <img
+                src="/assets/github.png"
+                alt="GitHub"
+                className="footerImage"
+              />
+            </a>
+          </div>
+        </footer>
+
+      </body>
     </html>
   );
 }
